6ad93e183bcddcbf74f9eea0c58cb5b8a4e65e1b
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2023-11-29 16:20:13 -0300

    Update produto.js

--- controllers/produto.js
+++ controllers/produto.js
@@ -1,28 +1,26 @@
-// const Produto = require("../models/produtos"); // Use um nome diferente para a variável
+ const Produto = require("../models/produtos"); // Use um nome diferente para a variável
 
-// exports.create = async (req, res) => {
-//   try {
-//     const categoria = req.body.categoria; // Corrija aqui para acessar os campos corretamente
-//     const nome = req.body.nome;
-//     const descricao = req.body.descricao;
-//     const preco = req.body.preco;
-//     const imagem = req.file.path;
+ exports.create = async (req, res) => {
+   try {
+     const categoria = req.body.categoria; // Corrija aqui para acessar os campos corretamente
+     const nome = req.body.nome;
+     const descricao = req.body.descricao;
+     const preco = req.body.preco;
+     const imagem = req.file.path;
 
-//     const novoProduto = new Produto({
-//       categoria,
-//       nome,
-//       descricao,
-//       preco,
-//       imagem
-//     });
-
-//     await novoProduto.save(); // Corrija aqui para chamar o método save() no novoProduto
-
-//     res.status(201).json({ novoProduto, message: "Produto salvo com sucesso" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Erro ao cadastrar produto" });
-//   }
-// };
+    const novoProduto = new Produto({
+       categoria,
+       nome,
+       descricao,
+       preco,
+       imagem
+     });
+     await novoProduto.save(); // Corrija aqui para chamar o método save() no novoProduto
+     res.status(201).json({ novoProduto, message: "Produto salvo com sucesso" });
+   } catch (error) {
+     res.status(500).json({ message: "Erro ao cadastrar produto" });
+   }
+ };
 
 // exports.find = async (req, res) => {
 //   try {
