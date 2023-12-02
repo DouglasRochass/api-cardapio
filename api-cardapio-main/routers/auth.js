e9fd1aa8b54fcb86a54bbe61f423d3f946363866
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2023-12-02 13:39:18 -0300

    Delete api-cardapio-main directory

--- api-cardapio-main/routers/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const autenticador = require('../controllers/autenticar')
-const router = express.Router()
-
-router.post('/', autenticador.Login)
-
-module.exports = router
-
